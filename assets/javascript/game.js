6576676a3dbcacf3d8e767c67237059d18ea4b39
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-09-03 16:56:24 -0700

    redid js code using new variables to declare that a player has been selected before moving into fight stage

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,149 @@
-$(document).ready (function() {
 
 var healthPoints;
 var baseAttackPower;
 var attackPower;
 var counterAttackPower;
-var moveImage;
+var selected;
+var defSelected;
+var charOneSelected = false;
+var charTwoSelected = false;
+var charOne;
+var charTwo;
 
-var luke = {
-    name: "Luke Skywalker",
-    healthPoints: 100,
-    attackPower: 10,
-    counterAttackPower: 20,
-    image: '<img src="assets/images/luke.jpeg" class="characterImages">'
-}
-var obiWan = {
-    name: "Obi-Wan Kenobi",
-    healthPoints: 100,
-    attackPower: 10,
-    counterAttackPower: 20,
-    image: '<img src="assets/images/obi-wan.jpeg" class="characterImages">'
-}
-var jarjar = {
-    name: "Jar Jar Binks",
-    healthPoints: 100,
-    attackPower: 10,
-    counterAttackPower: 20,
-    image: '<img src="assets/images/jarjar.jpeg" class="characterImages">'
-}
-var vadar = {
-    name: "Darth Vadar",
-    healthPoints: 100,
-    attackPower: 10,
-    counterAttackPower: 20,
-    image: '<img src="assets/images/vadar.jpeg" class="characterImages">'
-}
+// chracters objects -----------------------------------------------------------
+var characterList = [
+    luke = {
+        healthPoints: 100,
+        baseAttackPower: 0,
+        attackPower: 10,
+        counterAttackPower: 20,
+        selected: false,
+        defSelected: false
+    },
+    obiWan = {
+        healthPoints: 100,
+        baseAttackPower: 0,
+        attackPower: 10,
+        counterAttackPower: 20,
+        selected: false,
+        defSelected: false
+    },
+    jarjar = {
+        healthPoints: 100,
+        baseAttackPower: 0,
+        attackPower: 10,
+        counterAttackPower: 20,
+        selected: false,
+        defSelected: false
+    },
+    vadar = {
+        healthPoints: 100,
+        baseAttackPower: 0,
+        attackPower: 10,
+        counterAttackPower: 20,
+        selected: false,
+        defSelected: false
+    }]
 
-//gamestart and reset function
-function gameStart() {
+$(document).ready(function () {
 
-//reset each character images position
-    $(luke.image).appendTo("#allCharacters")
-    $(obiWan.image).appendTo("#allCharacters")
-    $(jarjar.image).appendTo("#allCharacters")
-    $(vadar.image).appendTo("#allCharacters")
+    //user character selection ------------------------------------------------
+    function characterSelection() {
+        $(".luke").on("click", function () {
+            if (charOneSelected === false) {
+                charOneSelected = true;
+                luke.selected = true;
+                charOne = luke;
+                $(".luke").appendTo(".fightDisplayL");
+                $(".obiWan, .jarjar, .vadar").css({ "border": "5px solid red" })
+            }
+        })
+        $(".obiWan").on("click", function () {
+            if (charOneSelected === false) {
+                charOneSelected = true;
+                obiWan.selected = true;
+                charOne = obiWan;
+                $(".obiWan").appendTo(".fightDisplayL");
+                $(".obiWan, .jarjar, .vadar").css({ "border": "5px solid red" })
+            }
+        })
+        $(".jarjar").on("click", function () {
+            if (charOneSelected === false) {
+                charOneSelected = true;
+                jarjar.selected = true;
+                charOne = jarjar;
+                $(".jarjar").appendTo(".fightDisplayL");
+                $(".obiWan, .jarjar, .vadar").css({ "border": "5px solid red" })
+            }
+        })
+        $(".vadar").on("click", function () {
+            if (charOneSelected === false) {
+                charOneSelected = true;
+                vadar.selected = true;
+                charOne = vadar;
+                $(".vadar").appendTo(".fightDisplayL");
+                $(".obiWan, .jarjar, .vadar").css({ "border": "5px solid red" })
+            }
+        })
+    }
 
-//reset each characters stats
-    luke.healthPoints = 100
-    luke.attackPower = 10
-    luke.counterAttackPower = 20
+    //enemy character selection -------------------------------------------------------------------
+    function enemySelection() {
+        $(".luke").on("click", function () {
+            if (charOneSelected === true && charTwoSelected === false && luke.selected != true) {
+                charTwoSelected = true;
+                luke.defSelected = true;
+                charTwo = luke;
+                $(".luke").appendTo(".fightDisplayR")
+            }
+        })
+        $(".obiWan").on("click", function () {
+            if (charOneSelected === true && charTwoSelected === false && obiWan.selected != true) {
+                charTwoSelected = true;
+                obiWan.defSelected = true;
+                charTwo = obiWan;
+                $(".obiWan").appendTo(".fightDisplayR")
+            }
+        })
+        $(".jarjar").on("click", function () {
+            if (charOneSelected === true && charTwoSelected === false && jarjar.selected != true) {
+                charTwoSelected = true;
+                jarjar.defSelected = true;
+                charTwo = jarjar;
+                $(".jarjar").appendTo(".fightDisplayR")
+            }
+        })
+        $(".vadar").on("click", function () {
+            if (charOneSelected === true && charTwoSelected === false && vadar.selected != true) {
+                charTwoSelected = true;
+                vadar.defSelected = true;
+                charTwo = vadar;
+                $(".vadar").appendTo(".fightDisplayR")
+            }
+        })
+    }
 
-    obiWan.healthPoints = 100
-    obiWan.attackPower = 10
-    obiWan.counterAttackPower = 20
+    //attack function --------------------------------------
+    function attackEnemy(){
+        $(".attack").on("click", function() {
+            if (charOneSelected === true && charTwoSelected === true) {
+                charOne.baseAttackPower += charOne.attackPower;
+                charOne.healthPoints -= charTwo.counterAttackPower;
+                charTwo.healthPoints -= charOne.baseAttackPower;
+                if (charOne.healthPoints < 0 && charTwo.healthPoints > 0) {
+                    alert("You win!")
+                }
+                else if (charTwo.healthPoints < 0 &&  charOne.healthPoints > 0) {
+                    alert("You lose!")
+                } 
 
-    jarjar.healthPoints = 100
-    jarjar.attackPower = 10
-    jarjar.counterAttackPower = 20
+            }
+        })
+    }
 
-    vadar.healthPoints = 100
-    vadar.attackPower = 10
-    vadar.counterAttackPower = 20
-}
 
-//calling gamestart function
-gameStart();
+    //restart function
 
-//selection of user character which will empty initial display and 
-//move new selection into your character and other options to enemy section
-$(jarjar.image).on("click", function () {
-    $("#allCharacters").empty()
-    $(jarjar.image).appendTo("#yourCharacter")
-})
-
+    enemySelection();
+    characterSelection();
 })
