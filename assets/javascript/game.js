b8816d67fbaceca5ce39e2ca71941e7fc6c6ffba
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-09-03 23:54:20 -0700

    added background image scaled to screen and game title

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ var charTwo;
 
 // chracters objects -----------------------------------------------------------
 var luke = {
-    name : "Luke Skywalker",
+    name: "Luke Skywalker",
     healthPoints: 120,
     baseAttackPower: 0,
     attackPower: 8,
@@ -21,7 +21,7 @@ var luke = {
     defSelected: false
 }
 var obiWan = {
-    name : "Obi-Wan Kenobi",
+    name: "Obi-Wan Kenobi",
     healthPoints: 100,
     baseAttackPower: 0,
     attackPower: 9,
@@ -30,7 +30,7 @@ var obiWan = {
     defSelected: false
 }
 var jarjar = {
-    name : "Jar Jar Binks",
+    name: "Jar Jar Binks",
     healthPoints: 150,
     baseAttackPower: 0,
     attackPower: 2,
@@ -39,7 +39,7 @@ var jarjar = {
     defSelected: false
 }
 var vadar = {
-    name : "Darth Vadar",
+    name: "Darth Vadar",
     healthPoints: 180,
     baseAttackPower: 1,
     attackPower: 1,
@@ -50,42 +50,53 @@ var vadar = {
 
 $(document).ready(function () {
 
+    $(".enemyLabel").hide()
+    $(".fightBoxLabel").hide()
+
     //user character selection ------------------------------------------------------------------
     function characterSelection() {
         $(".luke").on("click", function () {
             if (charOneSelected === false) {
+                $(".characterTitle").hide()
                 charOneSelected = true;
                 luke.selected = true;
                 charOne = luke;
                 $(".luke").appendTo(".fightDisplayL").css({ "background-color": "green" });
                 $(".obiWan, .jarjar, .vadar").appendTo(".enemyDisplay").css({ "background-color": "red" })
+                $(".enemyLabel").show()
             }
         })
         $(".obiWan").on("click", function () {
             if (charOneSelected === false) {
+                $(".characterTitle").hide()
                 charOneSelected = true;
                 obiWan.selected = true;
                 charOne = obiWan;
                 $(".obiWan").appendTo(".fightDisplayL").css({ "background-color": "green" });
                 $(".luke, .jarjar, .vadar").appendTo(".enemyDisplay").css({ "background-color": "red" })
+                $(".enemyLabel").show()
             }
         })
         $(".jarjar").on("click", function () {
             if (charOneSelected === false) {
+                $(".characterTitle").hide()
                 charOneSelected = true;
                 jarjar.selected = true;
                 charOne = jarjar;
                 $(".jarjar").appendTo(".fightDisplayL").css({ "background-color": "green" });
                 $(".obiWan, .luke, .vadar").appendTo(".enemyDisplay").css({ "background-color": "red" })
+                $(".enemyLabel").show()
             }
         })
         $(".vadar").on("click", function () {
             if (charOneSelected === false) {
+                $(".characterTitle").hide()
                 charOneSelected = true;
                 vadar.selected = true;
                 charOne = vadar;
                 $(".vadar").appendTo(".fightDisplayL").css({ "background-color": "green" });
                 $(".obiWan, .jarjar, .luke").appendTo(".enemyDisplay").css({ "background-color": "red" })
+                $(".enemyLabel").show()
             }
         })
     }
@@ -98,6 +109,7 @@ $(document).ready(function () {
                 luke.defSelected = true;
                 charTwo = luke;
                 $(".luke").appendTo(".fightDisplayR")
+                $(".fightBoxLabel").show()
             }
         })
         $(".obiWan").on("click", function () {
@@ -106,6 +118,7 @@ $(document).ready(function () {
                 obiWan.defSelected = true;
                 charTwo = obiWan;
                 $(".obiWan").appendTo(".fightDisplayR")
+                $(".fightBoxLabel").show()
             }
         })
         $(".jarjar").on("click", function () {
@@ -114,6 +127,7 @@ $(document).ready(function () {
                 jarjar.defSelected = true;
                 charTwo = jarjar;
                 $(".jarjar").appendTo(".fightDisplayR")
+                $(".fightBoxLabel").show()
             }
         })
         $(".vadar").on("click", function () {
@@ -122,6 +136,7 @@ $(document).ready(function () {
                 vadar.defSelected = true;
                 charTwo = vadar;
                 $(".vadar").appendTo(".fightDisplayR")
+                $(".fightBoxLabel").show()
             }
 
         })
@@ -131,7 +146,8 @@ $(document).ready(function () {
                 charOne.healthPoints = charOne.healthPoints - charTwo.counterAttackPower;
                 charTwo.healthPoints = charTwo.healthPoints - charOne.baseAttackPower;
                 $(".fightTextDisplay").text(charOne.name + " attacks " + charTwo.name + " for " + charOne.baseAttackPower + " , " + charTwo.name + " has " + charTwo.healthPoints + " HP remaining. "
-                + charTwo.name + " counter attacks " + charOne.name + " for " + charTwo.counterAttackPower + " , " + charOne.name + " has " + charOne.healthPoints + " HP remaining.")
+                    + charTwo.name + " counter attacks " + charOne.name + " for " + charTwo.counterAttackPower + " , " + charOne.name + " has " + charOne.healthPoints + " HP remaining.")
+                $(".fightTextDisplay").show()
                 if (charOne.healthPoints < 0) {
                     $(".fightTextDisplay").text(charOne.name + " has defeated " + charTwo.name + ". " + "You win!")
                 }
@@ -155,12 +171,18 @@ $(document).ready(function () {
 
         $(".luke, .obiWan, .jarjar, .vadar").appendTo(".characterBox").css({ "background-color": "green" })
 
+        $(".fightTextDisplay").hide()
+        $(".characterTitle").show()
+        $(".fightBoxLabel").hide()
+        $(".enemyLabel").hide()
+
         charOneSelected = false;
         charTwoSelected = false;
         charOne = {};
         charTwo = {};
 
         luke = {
+            name : "Luke Skywalker",
             healthPoints: 100,
             baseAttackPower: 0,
             attackPower: 10,
@@ -169,6 +191,7 @@ $(document).ready(function () {
             defSelected: false
         }
         obiWan = {
+            name : "Obi-Wan Kenobi",
             healthPoints: 110,
             baseAttackPower: 0,
             attackPower: 10,
@@ -177,6 +200,7 @@ $(document).ready(function () {
             defSelected: false
         }
         jarjar = {
+            name : "Jar Jar Binks",
             healthPoints: 120,
             baseAttackPower: 0,
             attackPower: 10,
@@ -185,6 +209,7 @@ $(document).ready(function () {
             defSelected: false
         }
         vadar = {
+            name : "Darth Vadar",
             healthPoints: 130,
             baseAttackPower: 0,
             attackPower: 10,
